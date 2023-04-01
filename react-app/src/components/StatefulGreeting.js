f434365bccef3bedb5ad88be8e3edf5214c3c8ec
Khubab <khubab220@gmail.com>
2023-04-01 16:17:45 +0000

    Add stateful component and fix async state value issue

--- react-app/src/components/StatefulGreeting.js
+++ react-app/src/components/StatefulGreeting.js
@@ -3,8 +3,33 @@ import React from "react";
 
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            intro: "Hello!",
+            btnText: "Exit"
+        }
+    }
+
+    handleClick() {
+        this.setState({
+            intro: "Goodbye!",
+            btnText: "Enter"
+        }, () => {
+            console.log("new state: ",this.state)
+        });
+        console.log("old state: ",this.state)
+    }
+
     render() {
-        return <div>Hello, I am {this.props.name}, {this.props.greeting}</div>
+        return (
+            <div>
+                <div>Stateful (Class) Component</div>
+                <div>{this.state.intro} {this.props.greeting}</div>
+                <button onClick={() => this.handleClick()}>{this.state.btnText}</button>
+            </div>
+        )
     }
 }
 
