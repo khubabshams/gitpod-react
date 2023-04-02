194c87244c1d97782ec77d0b3ab25076826e5c81
Khubab <khubab220@gmail.com>
2023-04-02 16:17:35 +0000

    Replcae code with one from es7 snippet

--- react-app/src/components/StatefulGreeting.js
+++ react-app/src/components/StatefulGreeting.js
@@ -1,9 +1,6 @@
+import React, { Component } from 'react'
 
-import React from "react";
-
-
-class StatefulGreeting extends React.Component {
-
+export class StatefulGreeting extends Component {
     constructor(props) {
         super(props);
         this.state = {
